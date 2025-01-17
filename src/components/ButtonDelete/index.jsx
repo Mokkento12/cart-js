752fa6198c7bc409fcc70bcbba6ebc52568f6cb5
roman alekseev <roma.alekseev.90@bk.ru>
2025-01-17 20:37:46 +0300

    Добавлены Count & ButtonDelete

---
+++ src/components/ButtonDelete/index.jsx
@@ -0,0 +1,9 @@
+const ButtonDelete = () => {
+  return (
+    <button type="button">
+      <img src="./img/icons/cross.svg" alt="Delete"></img>
+    </button>
+  );
+};
+
+export default ButtonDelete;
--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -1,3 +1,5 @@
+import ButtonDelete from "../ButtonDelete";
+import Count from "../Count";
 import "./style.scss";
 
 const Product = () => {
@@ -8,31 +10,11 @@ const Product = () => {
       </div>
       <div className="product__title">Apple MacBook Air 13</div>
       <div className="product__count">
-        <div className="count">
-          <div className="count__box">
-            <input
-              type="number"
-              className="count__input"
-              min="1"
-              max="100"
-              value="1"
-            />
-          </div>
-          <div className="count__controls">
-            <button type="button" className="count__up">
-              <img src="./img/icons/icon-up.svg" alt="Increase" />
-            </button>
-            <button type="button" className="count__down">
-              <img src="./img/icons/icon-down.svg" alt="Decrease" />
-            </button>
-          </div>
-        </div>
+        <Count />
       </div>
       <div className="product__price">110 000 руб.</div>
       <div className="product__controls">
-        <button type="button">
-          <img src="./img/icons/cross.svg" alt="Delete"></img>
-        </button>
+        <ButtonDelete />
       </div>
     </section>
   );
