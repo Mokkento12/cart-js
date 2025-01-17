4a5914fbccaa49bfea0757ca2e5a2efb3c6f96a4
roman alekseev <roma.alekseev.90@bk.ru>
2025-01-17 19:47:13 +0300

    Настроен компонент Title

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,15 @@
 import "./_vars.scss";
 import "./_reset.scss";
 import "./_base.scss";
+import "./_section-cart.scss";
+import Title from "../Title";
 
 function App() {
   return (
     <section className="section-cart">
       <header className="section-cart__header">
         <div className="container">
-          <h1 className="title-1">Корзина товаров</h1>
+          <Title />
         </div>
       </header>
       <div className="section-cart__body">
