173b2398bf0551664fc590a46f2f749627dd4374
roman alekseev <roma.alekseev.90@bk.ru>
2025-01-18 22:02:36 +0300

    Получение данных с помощью пропсов

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,12 +1,20 @@
 import CartHeader from "../CartHeader";
 import Product from "../Product";
 import CartFooter from "../CartFooter";
+import data from "./../../data.js";
+import { useState } from "react";
 
 const Cart = () => {
+  const [cart, setCart] = useState(data);
+
+  const products = cart.map((product) => {
+    return <Product product={product} />;
+  });
+
   return (
     <section className="cart">
       <CartHeader />
-      <Product />
+      {products}
       <CartFooter />
     </section>
   );
