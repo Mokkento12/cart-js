636b138fa44fcee886acfe03f4fc2cc59700b973
roman alekseev <roma.alekseev.90@bk.ru>
2025-01-19 11:49:31 +0300

    Деструктуризация элементов в product

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -3,14 +3,15 @@ import ButtonDelete from "../ButtonDelete";
 import "./style.scss";
 
 const Product = ({ product }) => {
+  const { img, title, price, count, id } = product;
   return (
     <section className="product">
       <div className="product__img">
-        <img src="./img/products/macbook.jpg" alt="Apple MacBook Air 13" />
+        <img src={`./img/products/${img}`} alt={title} />
       </div>
-      <div className="product__title">{product.title}</div>
+      <div className="product__title">{title}</div>
       <div className="product__count">{/* <Count /> */}</div>
-      <div className="product__price">{product.price}</div>
+      <div className="product__price">{price}</div>
       <div className="product__controls">
         <ButtonDelete />
       </div>
