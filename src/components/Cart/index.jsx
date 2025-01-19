b137d8f3fbb768cc450907807c2eeb6317b76ca9
roman alekseev <roma.alekseev.90@bk.ru>
2025-01-19 15:30:33 +0300

    Функция increase для увеличения товаров в корзине

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -11,12 +11,31 @@ const Cart = () => {
     setCart((cart) => cart.filter((product) => id !== product.id));
   };
 
+  const increase = (id) => {
+    console.log("Increase!", id);
+
+    setCart((cart) => {
+      return cart.map((product) => {
+        if (product.id === id) {
+          return {
+            ...product,
+            count: ++product.count,
+            priceTotal: product.count * product.price,
+          };
+        }
+
+        return product;
+      });
+    });
+  };
+
   const products = cart.map((product) => {
     return (
       <Product
         product={product}
         key={product.id}
         deleteProduct={deleteProduct}
+        increase={increase}
       />
     );
   });
--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -1,17 +1,19 @@
 import ButtonDelete from "../ButtonDelete";
-// import Count from "../Count";
+import Count from "../Count";
 import "./style.scss";
 
-const Product = ({ product, deleteProduct }) => {
-  const { img, title, price, count, id } = product;
+const Product = ({ product, deleteProduct, increase }) => {
+  const { img, title, priceTotal, count, id } = product;
   return (
     <section className="product">
       <div className="product__img">
         <img src={`./img/products/${img}`} alt={title} />
       </div>
       <div className="product__title">{title}</div>
-      <div className="product__count">{/* <Count /> */}</div>
-      <div className="product__price">{price}</div>
+      <div className="product__count">
+        <Count count={count} increase={increase} id={id} />
+      </div>
+      <div className="product__price">{priceTotal}</div>
       <div className="product__controls">
         <ButtonDelete deleteProduct={deleteProduct} id={id} />
       </div>
