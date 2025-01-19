57093fa1cfa0cf3ccdd79eee0cc7ff29c622e485
roman alekseev <roma.alekseev.90@bk.ru>
2025-01-19 16:17:20 +0300

    Фикс decrease

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -18,7 +18,7 @@ const Cart = () => {
           return {
             ...product,
             count: ++product.count,
-            priceTotal: product.count * product.price,
+            priceTotal: ++product.count * product.price,
           };
         }
 
@@ -31,10 +31,11 @@ const Cart = () => {
     setCart((cart) => {
       return cart.map((product) => {
         if (product.id === id) {
+          const newCount = product.count - 1 > 1 ? product.count - 1 : 1; // Обновляем count
           return {
             ...product,
-            count: product.count - 1 > 1 ? --product.count : 1,
-            priceTotal: product.count * product.price,
+            count: newCount,
+            priceTotal: newCount * product.price, // Пересчитываем priceTotal с новым count
           };
         }
 
