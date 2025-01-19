0956c1caca33bb93e9270b3e7640cdf3fa36352d
roman alekseev <roma.alekseev.90@bk.ru>
2025-01-19 12:12:04 +0300

    Добавлена функция удаления товара

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -7,8 +7,18 @@ import { useState } from "react";
 const Cart = () => {
   const [cart, setCart] = useState(data);
 
+  const deleteProduct = (id) => {
+    setCart((cart) => cart.filter((product) => id !== product.id));
+  };
+
   const products = cart.map((product) => {
-    return <Product product={product} key={product.id} />;
+    return (
+      <Product
+        product={product}
+        key={product.id}
+        deleteProduct={deleteProduct}
+      />
+    );
   });
 
   return (
